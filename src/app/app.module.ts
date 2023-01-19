b2984917f35fe14788d00f6a47cda9e353ae816b
Bimsara <bimsara125@gmail.com>
2023-01-19 23:59:07 +0530

    commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { LibraryDashboardComponent } from './library-dashboard/library-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    LibraryDashboardComponent
   ],
   imports: [
     BrowserModule,
